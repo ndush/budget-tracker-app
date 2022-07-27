399f69fe432dd1c68704a1bcba544caf709c84eb
damaris <mdamarisnduku@gmail.com>
2022-07-28 01:40:35 +0300

    add sign and color to signify expense or income

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Transaction({ transaction }){
+    const sign = transaction.amount < 0 ? '-' : '+';
     return (
         <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
           {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
