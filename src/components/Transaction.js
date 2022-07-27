2c5e4ca3ee5ef4001865937b9cea37759ffc3475
damaris <mdamarisnduku@gmail.com>
2022-07-28 02:43:07 +0300

    add delete functionality

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 function Transaction({ transaction }){
+    const { deleteTransaction } = useContext(GlobalContext);
+    
     const sign = transaction.amount < 0 ? '-' : '+';
     return (
         <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
